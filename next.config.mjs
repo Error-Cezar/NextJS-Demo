154018eba55c90e037a57b46342e92104a31a98e
Error-Cezar <64325581+Error-Cezar@users.noreply.github.com>
2024-07-18 11:55:45 +0200

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,13 @@
+// NO MORE COMMENTING FOR LOCAL USE (proud of that)
+
 /** @type {import('next').NextConfig} */
 
 const isProd = process.env.NODE_ENV === 'production'
 
-// COMMENT THE CONTENT OF NEXTCONFIG FOR LOCAL USAGE
 const nextConfig = {
   reactStrictMode: isProd ? true : false,
   basePath: isProd ? '/NextJS-Demo' : '',
   assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH || ''
 }
-// COMMENT THE CONTENT OF NEXTCONFIG FOR LOCAL USAGE
 
 export default nextConfig;
